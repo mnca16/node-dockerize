0ed0fe2eba8bea646d93fab7f56a18b3afef1df6
Monica Delgado <mncawebdev@gmail.com>
2023-10-03 22:49:17 -0500

    updated index.js file

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const { readFile } = require('fs').promises;
 
-const app = express()
-const PORT = 4000
+app.get('/', async (request, response) => {
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+    response.send( await readFile('./home.html', 'utf8') );
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
-
-// Export the Express API
-module.exports = app
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
