c0541ec63407753598dafc115c2830f9ad592adf
Monica Delgado <mncawebdev@gmail.com>
2023-10-03 21:39:39 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+const express = require('express')
+const { readFile} = require('fs')
+
+const app = express()
+console.log(process.env.API_TOKEN)
+app.get('/', (request, response) => {
+    readFile('./home.html', 'utf8', (err, html) => {
+        if(err) {
+            response.status(500).send('sorry, out of order')
+        }
+        response.send(html)
+    })
+})
+
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
